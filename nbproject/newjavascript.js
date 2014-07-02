f029f097c27e80e8459b027e5ea8f8cd9aba8411
kobalc1 <kobalc1@IR1-PC-08-W7.schule.local>
2014-07-02 17:19:15 +0200

    grundstruktur angelegt

---
+++ nbproject/newjavascript.js
@@ -0,0 +1,19 @@
+$("document").ready(function(){
+
+$("body").append("<b>!BMI Rechner!</b><br>");
+$("body").append("Größe in m:<input type='text' id='groese'><br>");
+$("body").append("Gewicht in kg:<input type='text' id='gewicht'><br>");
+$("body").append("<input type='button' id='knopf' value='Berechne BMI'><br>");
+
+$("#knopf").click(function(){
+  var groesse=$("#groese").val();
+  var gewicht=$("#gewicht").val();
+  var bmi=0;
+  var g=groesse^2;
+  
+  bmi=gewicht/g;
+ $("body").append("Ihr BMI beträgt "+bmi+"");
+});
+});
+ 
+  
